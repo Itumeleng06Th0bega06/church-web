77815430838ede308d3396eff47ccb434a790775
#ItomicStudiosLines <146145335+Itumeleng06Th0bega06@users.noreply.github.com>
2025-02-03 18:31:03 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 import time
 
 # Set up the page
-st.set_page_config(page_title="SBOI", page_icon='assets/Shekinah_logo.png')
+st.set_page_config(page_title="SBOI")
 
 # Load CSS
 try:
@@ -15,7 +15,6 @@ except Exception as e:
     st.error(f"An error occurred while loading the CSS: {e}")
 
 # Constants
-LOGO_PATH = "assets/Shekinah_logo.png"
 CHURCH_COVENANT = """
 **:orange[Church Covenant]**
 
@@ -44,9 +43,6 @@ VISION_DEDICATION = """
 'Heavenly Father, We stand before You as a church, united in purpose and committed to Your vision. We dedicate the new season of this ministry to You. Guide us as we navigate this journey with faith and wisdom. Amen.'
 """
 
-def show_logo():
-    st.image(LOGO_PATH, use_container_width=True)
-
 def show_welcome_message():
     st.title(":orange[Lord Remember Me]")
     st.write("Judges 16:28")
@@ -67,7 +63,6 @@ def Home():
         tab1, tab2, tab3 = st.tabs(['Home', 'Ministries', "Church Members"])
         with tab1:
             with st.container():
-                show_logo()
                 show_welcome_message()
         with tab2:
             if "index" not in st.session_state:
@@ -83,98 +78,5 @@ def Home():
                 with col3:
                     if st.button("Women", use_container_width=True):
                         st.session_state.index = "Women"
-                with col4:
-                    if st.button("Gentlemen", use_container_width=True):
-                        st.session_state.index = "Gentlemen"
-
-                if st.session_state.index == "Children":
-                    st.markdown("<h1><center>Kingdom minded Kids</center></h1>", unsafe_allow_html=True)
-                    st.markdown("<h4><center>Every Saturday @ 9am</center></h4>", unsafe_allow_html=True)
-                    st.image("children/10.jpg", use_container_width=True)
-                    try:
-                        with open('children/A1.mp4', "rb") as vid:
-                            st.video(vid.read())
-                    except FileNotFoundError:
-                        st.error("Video file not found.")
-                    images = ['children/1.jpg', 'children/2.jpg', 'children/3.jpg', 'children/4.jpg', 'children/5.jpg', 'children/6.jpg', 'children/7.jpg', 'children/8.jpg', 'children/9.jpg', 'children/11.jpg']
-                    container = st.empty()
-                    index = 0
-                    while True:
-                        with container:
-                            st.image(images[index], use_container_width=True)
-                        index += 1
-                        if index >= len(images):
-                            index = 0
-                            break
-                        time.sleep(5)
-                        container.empty()
-
-                if st.session_state.index == "Youth":
-                    st.title('Youth')
-                    st.image("assets/y1.png")
-                    st.write("**Welcome To Shekinah Blaze Outreach International Youth, Where We Are Nurtured To Being God Fearing Kingdom Leaders Through The Unwavering Teachings Of Biblical Principles.**")
-                    st.write("**About Us__Shekinah Blaze Is A Youth Group For Teens And Young Adults. Our Mission Is To Extend God's Love And Teachings To The Unchurched, Empower Young Believers To Live A Christ-Centered Life, And Prepare Them For Leadership Roles Within The Church And Community.**")
-                    st.write("**:orange[. Live By Faith]**")
-                    st.write("**:orange[. Build Meaningful Relationships]**")
-                    st.write("**:orange[. Develop Leadership Skills]**")
-                    st.write("**:orange[. Engage In Community Service]**")
-                    st.write("**:orange[. And Lastly Have Fun]**")
-                    st.image("assets/y2.png")
-                    st.write("**:orange[EMPOWERED BY GOD, ENERGIZED BY COMMUNITY]**")
-                    st.write("It’s all about BELONGING… To your FAMILY... To your CHURCH FAMILY... To the FAMILY OF GOD")
-                    st.image("assets/y3.png")
-                    st.image("assets/y4.jpg")
-
-                if st.session_state.index == "Women":
-                    st.title('Women')
-
-                if st.session_state.index == "Gentlemen":
-                    st.title("Gentlemen")
-                    st.image("assets/mens conference.jpg")
-        with tab3:
-            st.write(":orange[Key duties of every Church member:]")
-            st.write("""
-1. Attending Services Regularly
-Make it a priority to attend church services consistently, participating in worship, teachings, 
-and fellowship with other believers to grow spiritually.
-
-2. Engaging in Personal Spiritual Growth
-Commit to personal spiritual practices such as prayer, Bible study, and seeking God’s 
-guidance, nurturing a relationship with God beyond just attending services.
-
-3. Serving in Church Ministries
-Volunteer in church ministries according to individual gifts and talents, contributing to the 
-Church’s mission and supporting others within the community.
-
-4. Supporting the Church Vision and Leadership
-Align with the Church’s vision and uphold respect for its leaders, encouraging unity, 
-supporting initiatives, and contributing to the church’s goals.
-
-5. Giving Financially and Generously
-Contribute financially through tithes and offerings to support the Church’s operations, 
-missions, and outreach programs, giving as an act of worship and stewardship.
-
-6. Participating in Fellowship and Building Community
-Build relationships with other Church members, engaging in fellowship, small groups, or 
-Church events, fostering a strong, supportive Christian community.
-
-7. Living as a Positive Example
-Live a life that reflects Christian values in personal conduct, relationships, and decision making, being a witness to others both inside and outside the Church.
-
-8. Evangelizing and Inviting Others
-Share the gospel and invite others to Church, helping to spread God’s Word and welcome 
-new believers into the community.
-
-9. Praying for the Church and its Members
-Pray regularly for the Church’s vision, leadership, ministries, and fellow members, 
-interceding for God’s guidance, protection, and blessing over the Church.
-
-10. Upholding Unity and Avoiding Conflict
-Promote unity by resolving conflicts respectfully, speaking positively about the Church, and 
-avoiding gossip or divisive behavior, fostering peace within the Church family.
-
-:orange[These duties enable each Church member to actively contribute to the 
-growth, health, and mission of the Church community]
-""")
 
 Home()
