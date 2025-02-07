4e06c8417fe87813cee32111ebac4ee31f99b5ce
#ItomicStudiosLines <146145335+Itumeleng06Th0bega06@users.noreply.github.com>
2025-02-07 13:55:35 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -18,7 +18,7 @@ def Home():
                                 
                                 st.write("Judges 16:28")
                                 st.image("25.2.jpg",use_container_width=True)
-                                st.markdown("<h1><center>Welcome To Shekinah Blaze Outreach International</center></h1>",unsafe_allow_html=True)
+                                st.markdown("<h4><center>Welcome To Shekinah Blaze Outreach International</center></h4>",unsafe_allow_html=True)
                                 st.markdown("""<center>A Church With A Family Concept.</center>""",unsafe_allow_html=True)
                                 st.image('25.jpg',use_container_width=True)
                                 st.image('15a.jpg',use_container_width=True)
