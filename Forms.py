3be5e4e2c5ddb76f066a4b3978af29ccce658e14
#ItomicStudiosLines <146145335+Itumeleng06Th0bega06@users.noreply.github.com>
2025-02-11 06:36:35 +0200

    Add files via upload

--- Forms.py
+++ Forms.py
@@ -12,12 +12,12 @@ def form():
         col1,col2=st.columns(2)
         with col1:
             name=st.text_input('enter Name',placeholder='Name *',label_visibility='hidden')
-            surname=st.text_input('Surname',placeholder='Surname *',label_visibility='hidden')
+            email=st.text_input('Email',placeholder='Email',label_visibility='hidden')
             gender=st.radio("Gender *",['Male','Female'],index=None)
             about = st.radio("How Did You Hear About Us",['Social Media','Direct Invitation'],index=None,horizontal=True)
         with col2:
+            surname=st.text_input('Surname',placeholder='Surname *',label_visibility='hidden')
             contact=st.text_input("Contact",placeholder='Contact *',label_visibility='hidden')                   
-            email=st.text_input('Email',placeholder='Email',label_visibility='hidden')
             status=st.radio("Marital Status",['Single','Married'],index=None)
             attendance = st.radio("Have You Visited Our Church Before?",['First Timer','Regular Attendee','Interested In Becoming A Member'],horizontal=True,index=None)
         social = st.selectbox("How Can We Contact You",['Call','WhatsApp','Facebook'],placeholder='Options',index=None)
@@ -29,11 +29,11 @@ def form():
                 if name and surname and contact and gender:
                         if contact.isdigit() and len(contact) == 10:
                                 data(name,surname,email,gender,contact,status,attendance,about,social,comments)
-                                st.write(f":orange[Thank You] {name}  {surname}")
+                                st.write(f"Thank You {name}  {surname}")
                         else:
-                              st.write('**:red[invalid contact!]**
+                              st.write('invalid contact!')        
                 else:
-                        st.write(':red[Please fill all the required spaces marked]')
+                        st.warning('Please fill all the required spaces marked **')
                 
             
 def data(a,b,c,d,e,f,g,h,i,j):
