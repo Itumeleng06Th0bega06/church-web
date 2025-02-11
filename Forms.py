59ede5bc625c26e8cd6362e8f008c41f2bbef41c
#ItomicStudiosLines <146145335+Itumeleng06Th0bega06@users.noreply.github.com>
2025-02-11 05:26:09 +0200

    Update Forms.py

--- Forms.py
+++ Forms.py
@@ -33,7 +33,7 @@ def form():
                         else:
                               st.write('**:red[invalid contact!]**')        
                 else:
-                        st.write(':orange[Please fill all the required spaces marked]')
+                        st.write(':red[Please fill all the required spaces marked]')
                 
             
 def data(a,b,c,d,e,f,g,h,i,j):
