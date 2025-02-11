9088deebcc36145a2e7d36247b4e3e189ef83cab
#ItomicStudiosLines <146145335+Itumeleng06Th0bega06@users.noreply.github.com>
2025-02-11 16:24:40 +0200

    Update Forms.py

--- Forms.py
+++ Forms.py
@@ -12,12 +12,12 @@ def form():
         col1,col2=st.columns(2)
         with col1:
             name=st.text_input('enter Name',placeholder='Name *',label_visibility='hidden')
-            email=st.text_input('Email',placeholder='Email',label_visibility='hidden')
+            surname=st.text_input('Surname',placeholder='Surname *',label_visibility='hidden')
             gender=st.radio("Gender *",['Male','Female'],index=None)
             about = st.radio("How Did You Hear About Us",['Social Media','Direct Invitation'],index=None,horizontal=True)
         with col2:
-            surname=st.text_input('Surname',placeholder='Surname *',label_visibility='hidden')
-            contact=st.text_input("Contact",placeholder='Contact *',label_visibility='hidden')                   
+            contact=st.text_input("Contact",placeholder='Contact *',label_visibility='hidden',max_chars=10)                   
+            email=st.text_input('Email',placeholder='Email',label_visibility='hidden')
             status=st.radio("Marital Status",['Single','Married'],index=None)
             attendance = st.radio("Have You Visited Our Church Before?",['First Timer','Regular Attendee','Interested In Becoming A Member'],horizontal=True,index=None)
         social = st.selectbox("How Can We Contact You",['Call','WhatsApp','Facebook'],placeholder='Options',index=None)
@@ -29,11 +29,11 @@ def form():
                 if name and surname and contact and gender:
                         if contact.isdigit() and len(contact) == 10:
                                 data(name,surname,email,gender,contact,status,attendance,about,social,comments)
-                                st.write(f"Thank You {name}  {surname}")
+                                st.success(f"**:orange[Thank You]** {name}  {surname}")
                         else:
-                              st.write('invalid contact!')        
+                              st.warning('invalid contact!')      
                 else:
-                        st.warning('Please fill all the required spaces marked **')
+                        st.warning('Please fill all the required spaces marked *')
                 
             
 def data(a,b,c,d,e,f,g,h,i,j):
@@ -41,6 +41,6 @@ def data(a,b,c,d,e,f,g,h,i,j):
     cur.execute("INSERT INTO my_table VALUES (?,?,?,?,?,?,?,?,?,?)",(a,b,c,d,e,f,g,h,i,j))
     conn.commit()
     conn.close()
-    st.success('submited')
+    
     
 form()
