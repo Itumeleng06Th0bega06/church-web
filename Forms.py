c9fb443ce811c49429494e6f8a9c47d3c9ae3670
#ItomicStudiosLines <146145335+Itumeleng06Th0bega06@users.noreply.github.com>
2025-02-11 06:35:15 +0200

    Update Forms.py

--- Forms.py
+++ Forms.py
@@ -27,7 +27,7 @@ def form():
 
         if submit:
                 if name and surname and contact and gender:
-                        if contact.isdigit() and len(contact)==10:
+                        if contact.isdigit() and len(contact) == 10:
                                 data(name,surname,email,gender,contact,status,attendance,about,social,comments)
                                 st.write(f":orange[Thank You] {name}  {surname}")
                         else:
