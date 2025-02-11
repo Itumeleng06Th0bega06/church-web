eb33d398003c26e124c2727507ffd315c3b1b1d6
#ItomicStudiosLines <146145335+Itumeleng06Th0bega06@users.noreply.github.com>
2025-02-11 04:51:54 +0200

    Update Vision And Mission.py

--- Vision And Mission.py
+++ Vision And Mission.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
 col1,col2=st.columns(2,gap='small',vertical_alignment='top')
-col1.image('l1.jpg',width=100)
-col2.image('l2.jpg',width=100)
+
 col1.subheader(":orange[Vision.]")
 col1.write(""":orange[Raising God Fearing Kingdom Leaders
            By Teaching Uncompromised Bible Principles.]""")
@@ -31,22 +30,24 @@ col2.write("""•The Church will be involved in crime prevention initiatives and
                         Biblical values and by assisting to re-incorporate them back to their 
                         communities. The Church will also provide spiritual counseling to the 
                         inmates.""")
-col1.image('l4.jpg',width=100)
-col2.image('l6.jpg',width=100)
+
 st.write("""•The Church will also be involved in hospital Ministry by visiting hospitals to pray for 
                         the sick and providing spiritual guidance to the sick. The Church intends to build a 
                         centre for the terminally ill so as to have them close to the Church for prayers and 
                         spiritual support. Doctors and nurses will be on hand to provide necessary medical 
                         assistance. Trained care-givers will run the centre on a fulltime basis. Pastors and 
                         trained counselors will provide counseling on a regular basis.""")
+
 st.write("""•The Church intends to reach out to the prostitution community to lead them to 
                         Christ in order to reduce the HIV/AIDS pandemic. The Church will provide counseling 
                         and spiritual guidance to those who want to change their ways. The Church will have 
                         a shelter for such people who are seeking refuge so as to mend their lives.""")
+
 st.write("""•The Church will have a shelter for the destitute and the abused. The Church will 
                         house women and children who need shelter and children until such time that they 
                         find homes or places where they can live normal lives. The Church will help look after 
                         HIV/AIDS orphans.""")    
+
 st.write("""•The Church will raise funds to achieve its objectives through donations from 
                         individuals and organizations that share or support our goals. The Church will also 
                         raise funds through campaigns and sponsorships from governmental and private 
