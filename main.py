ade2a2ebb09f2dd4dc57dd2158647ffdfae64b94
#ItomicStudiosLines <146145335+Itumeleng06Th0bega06@users.noreply.github.com>
2025-02-13 18:29:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import pathlib
 # Set up the page
 st.set_page_config(page_title="SBOI",
                    layout='centered',
-                   page_icon='Shekinah_logo.png')
-                   theme = 'light' 
+                   page_icon='Shekinah_logo.png', 
+                   theme = 'light') 
 
 
 home = st.Page(
