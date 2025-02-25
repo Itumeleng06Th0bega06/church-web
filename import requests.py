29aa6a10bf3d9df4b4f92f32f61d6fdaf8afb484
#ItomicStudiosLines <146145335+Itumeleng06Th0bega06@users.noreply.github.com>
2025-02-25 03:05:59 +0200

    Update import requests.py

--- import requests.py
+++ import requests.py
@@ -1,7 +1,6 @@
 ﻿import requests
-     import time
-
-     while True:
-         response = requests.get("https://your-streamlit-app-url.streamlit.app/")
+import time
+while True:
+         response = requests.get("https://shekinahblazeoi.streamlit.app/")
          print(f"Pinged app at {time.ctime()}, Status Code: {response.status_code}")
          time.sleep(600)  # Ping every 10 minutes
